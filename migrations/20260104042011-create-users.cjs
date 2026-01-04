23311c201845ec98253ab34dc33b5206a13da573
ericsmith92 <ericsmth884@gmail.com>
2026-01-03 23:33:11 -0500

    feat: wire up sequelize and run users migration

---
+++ migrations/20260104042011-create-users.cjs
@@ -0,0 +1,42 @@
+"use strict";
+
+/** @type {import("sequelize-cli").Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+
+      display_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("now"),
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("users");
+  },
+};
